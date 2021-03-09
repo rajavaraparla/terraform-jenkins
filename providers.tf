2c86b164526bf6308c3e5737ef6deeca89a16bfc
NAGARAJU <varaparla.raja@gmail.com>
2021-03-10 00:18:38 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -4,9 +4,8 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "javahome-tf-nv-1234567"
+    bucket = "javahome-tf-1212"
     key    = "terraform.tfstate"
-    region = "${var.region}"
-    dynamodb_table = "javahome5-tf"
+    region = "ap-south-1"
   }
 }
